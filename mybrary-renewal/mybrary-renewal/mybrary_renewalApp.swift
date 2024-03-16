1c561f4fde163b89bd21e6da77aa5b3728a3c99c
DonggyuJin <jdk2531@gmail.com>
2024-03-16 18:12:07 +0900

    [Master] 마이브러리 리뉴얼

---
+++ mybrary-renewal/mybrary-renewal/mybrary_renewalApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybrary_renewalApp.swift
+//  mybrary-renewal
+//
+//  Created by dongs on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct mybrary_renewalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
