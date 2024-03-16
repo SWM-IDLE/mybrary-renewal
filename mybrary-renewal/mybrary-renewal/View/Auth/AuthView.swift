7769f53fd5e8fc8d6aab9fbb5e3063f84dd2fd34
DonggyuJin <jdk2531@gmail.com>
2024-03-16 18:43:44 +0900

    [IDLE-321] feat: AuthView 생성

---
+++ mybrary-renewal/mybrary-renewal/View/Auth/AuthView.swift
@@ -0,0 +1,14 @@
+//
+//  AuthView.swift
+//  mybrary-renewal
+//
+//  Created by dongs on 3/16/24.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text("로그인")
+    }
+}
